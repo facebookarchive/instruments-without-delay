6f7c4087c347d7ab1085fa646a2c61669c1502f3
Fred Potter <fpotter@fb.com>
2013-02-27 17:13:09 -0500

    rename 'instruments-without-delay' to just 'instruments'
    This way people can just manipulate their PATH to make their stuff use
    this script instead of /usr/bin/instruments

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ xcodebuild \
   -configuration Release \
   CONFIGURATION_BUILD_DIR=$BUILD_OUTPUT_DIR
 
-cp instruments-without-delay $BUILD_OUTPUT_DIR/instruments-without-delay
+cp instruments $BUILD_OUTPUT_DIR/instruments
 
 if [[ $1 == "test" ]]; then
   TEST_JS=$(echo `pwd`/test.js)
@@ -19,7 +19,7 @@ if [[ $1 == "test" ]]; then
 
   OUTPUT_DIR=$(/usr/bin/mktemp -d -t trace)
   pushd $OUTPUT_DIR
-  $BUILD_OUTPUT_DIR/instruments-without-delay \
+  $BUILD_OUTPUT_DIR/instruments \
     -t "$XCODE_PATH"/../Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate \
     $BUILD_OUTPUT_DIR/TestApp.app \
     -e UIASCRIPT $TEST_JS
